c70c78213a7b3e5c502a41e4344c9ca489a00b45
Brandon L Craig <bcraig2612@gmail.com>
2022-03-21 09:38:38 -0700

    Attempt to resolve CORS issue.

--- src/App.js
+++ src/App.js
@@ -87,13 +87,13 @@ function App() {
   const [bookings, setBookings] = useState([]);
 
   useEffect(() => {
-    Axios.get("https://spruce-booking.herokuapp.com/https://spruce-booking.netlify.app/api/getBookings").then((response) => {
+    Axios.get("https://spruce-booking.herokuapp.com/api/getBookings").then((response) => {
       setBookings(response.data);
     })
   }, [bookings])
 
   const submitBooking = () => {
-    Axios.post("https://spruce-booking.herokuapp.com/https://spruce-booking.netlify.app/api/createBooking", {
+    Axios.post("https://spruce-booking.herokuapp.com/api/createBooking", {
       customerName: customerName,
       emailAddress: emailAddress,
       streetAddress: streetAddress,
